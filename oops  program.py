811d503156b0ccdc9b44146c4fb4fe74a9b5d4e7
punamchand wagh <punamchandwagh@gmail.com>
2025-12-31 08:39:24 +0530

    Update oops  program.py

--- oops  program.py
+++ oops  program.py
@@ -1,7 +1,12 @@
-# What is self in Python?
+# 1 What is self in Python:--> self refers to the current object.
 
 class Demo:
     def show(self):
         print("Hello")
 
+# 2 What is __init__():-->It is a constructor that runs automatically when an object is created.
 
+class Person:
+    def __init__(self, name):
+        self.name = name
+ #       
